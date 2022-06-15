38f44fa5b90f9a4d18c48ce8ddaa19d80ea19cae
VladimirVolvakov <dr.volvakov@gmail.com>
2022-06-15 17:07:20 +0300

    feat: add monthly payment calculation functionality (app.js)

--- app.js
+++ app.js
@@ -10,11 +10,24 @@ function getResults (event) {
     const amount = document.querySelector('#amount').value;
     const interest = document.querySelector('#interest').value;
     const period = document.querySelector('#period').value;
-    const monthlyPayment = document.querySelector('#monthly-payment');
-    const totalPayment = document.querySelector('#total-payment');
-    const totalInterest = document.querySelector('#total-interest');
+    const monthlyPaymentOutput = document.querySelector('#monthly-payment');
+    const totalPaymentOutput = document.querySelector('#total-payment');
+    const totalInterestOutput = document.querySelector('#total-interest');
 
-    console.log(amount, interest, period);
+    // Parse string to decimal values:
+    const sumOfLoan = parseFloat(amount);
+    const interestPerMonth = parseFloat(interest) / (100 * 12);
+    const paymentPeriodInMonths = parseFloat(period) * 12;
+
+    // Monthly payment calculation:
+    const yield = Math.pow(1 + interestPerMonth, paymentPeriodInMonths);
+    const monthlyPayment = (sumOfLoan * yield * interestPerMonth) / (yield - 1);
+
+    // Check if monthly payment is a finite number:
+    if (isFinite(monthlyPayment)) {
+        monthlyPaymentOutput.value = monthlyPayment.toFixed(2);
+    }
+
     // Prevent default behaviour of form after being submitted:
     event.preventDefault();
 };
